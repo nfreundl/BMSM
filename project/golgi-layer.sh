1ef3b367e4a60307ad699a288042480d9db34024
nfreundl <nicolas.freundler@epfl.ch>
2018-05-15 23:27:51 +0200

    advanced the  first step, stuck in the second step

---
+++ project/golgi-layer.sh
@@ -0,0 +1,2 @@
+python  insane.py -f  1fj2-martinized.gro -o golgi-membrane.gro  -p topol-golgi.top  -l POPC:50  -l POPE:20  -l POPI:12 -sol PW  -box  13,13,19  -pbc square  -dm 6
+
---
+++ project/martinize-protein.sh
@@ -0,0 +1,1 @@
+python martinize.py  -f 1fj2.pdb  -o topol.top  -x 1fj2-martinized.pdb  -n index.ndx  -nmap map.ndx  -p Backbone  -pf 1000  -ff elnedyn22p
